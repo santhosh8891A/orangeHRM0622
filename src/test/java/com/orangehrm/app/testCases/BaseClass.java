2d7fe28b7f6e99b6950ab09d61b2b5ac561db61f
Santhosh <santhosh8891@gmail.com>
2024-06-24 17:13:27 +0530

    Updated the cod 0624

--- src/test/java/com/orangehrm/app/testCases/BaseClass.java
+++ src/test/java/com/orangehrm/app/testCases/BaseClass.java
@@ -1,42 +1,59 @@
 package com.orangehrm.app.testCases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.orangehrm.app.utitlities.ReadConfig;
+import com.orangehrm.app.utitlities.Webdriverutils;
 
 public class BaseClass {
 
 	public ReadConfig readconfig;
-	public static WebDriver driver=null;
+	public static WebDriver driver = null;
 	public String applicationURL;
 	public String userName;
 	public String password;
 	public String title;
-
+	public Webdriverutils webdrutils;
+    public static Logger logger;
+	@Parameters("browser")
 	@BeforeSuite
-	public void setUp() {
-		//Reading data from properties file
-		//Add a new code
-		readconfig=new ReadConfig();
-		applicationURL=readconfig.getFromConfingFile("ApplicationURL");
-		userName=readconfig.getFromConfingFile("username");
-		password=readconfig.getFromConfingFile("password");
-		title=readconfig.getFromConfingFile("Title");
-		
-		
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--start-maximized");
-		driver = new ChromeDriver(options);
+	public void setUp(String browser) {
+		// Reading data from properties file
+		// Add a new code
+		readconfig = new ReadConfig();
+		applicationURL = readconfig.getFromConfingFile("ApplicationURL");
+		userName = readconfig.getFromConfingFile("username");
+		password = readconfig.getFromConfingFile("password");
+		title = readconfig.getFromConfingFile("Title");
+		webdrutils=new Webdriverutils();
+		if (browser.contentEquals("Chrome"))
+
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			driver = new ChromeDriver(options);
+		} else {
+			driver = new EdgeDriver();
+		}
+		logger = Logger.getLogger("orangeHRM");
+		PropertyConfigurator.configure("Log4j.properties");
+
 		driver.get(applicationURL);
 
 	}
 
 	@AfterSuite
 	public void tearDown() {
+		driver.quit();
+		System.out.println("Test Execution Completed");
 
 	}
 
--- src/test/java/com/orangehrm/app/testCases/LoginPageTC_001.java
+++ src/test/java/com/orangehrm/app/testCases/LoginPageTC_001.java
@@ -1,24 +1,32 @@
 package com.orangehrm.app.testCases;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.testng.annotations.Test;
 
 import com.orangehrm.app.pageobjects.LoginPage;
+import com.orangehrm.app.utitlities.Webdriverutils;
 
 import junit.framework.Assert;
 
 public class LoginPageTC_001 extends BaseClass {
 	
 	@Test
-	public void loginValidation()
+	public void loginValidation() throws IOException, InterruptedException
 	{
-		//Createdthe login page object
+		//Created the login page object
 		 LoginPage lp=new LoginPage(driver);
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		 lp.login(userName,password);
+		 logger.info("Entered User name");
+		 logger.info("Entered Password");
 		 String expectedTitle=driver.getTitle();
+		 logger.info("Validated Title");
+		 Thread.sleep(5000);		 
+		 webdrutils.captureScreen(driver, "Login Funcationality");
 		 Assert.assertEquals(title, expectedTitle);
+		 
 		 
 	}
 	
