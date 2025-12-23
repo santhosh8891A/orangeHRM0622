397e5279f62482041223abd501d1cbe76dca7d24
Santhosh <santhosh8891@gmail.com>
2025-12-23 20:54:36 +0530

    This is new page for Emergency contacts

---
+++ src/test/java/com/orangehrm/app/pageobjects/EmergencyContactsPage.java
@@ -0,0 +1,14 @@
+package com.orangehrm.app.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmergencyContactsPage {
+	WebDriver ldriver;
+
+	//Parameterized constructor created on 0910 for testing 
+	public EmergencyContactsPage(WebDriver rdriver) {
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+}
